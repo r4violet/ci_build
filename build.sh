441a3e0eb02430ee3e952dee0268dd60b4769160
Brock5555 <77067332+Brock5555@users.noreply.github.com>
2021-04-11 22:31:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,19 +8,19 @@ cd /tmp/recovery
 
 sudo apt install git -y
 
-repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault 
+repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault 
 
 repo sync -j$(nproc --all)
 
-git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git device/xiaomi/violet
+git clone https://github.com/Brock5555/device_realme_RMX2185 device/realme/RMX2185
 
 rm -rf out
 
-. build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
+. build/envsetup.sh && lunch omni_RMX2185-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 
-cd out/target/product/violet
+cd out/target/product/RMX2185
 
 curl -sL https://git.io/file-transfer | sh 
 
